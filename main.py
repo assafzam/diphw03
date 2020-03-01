eae5b991ccfc1766cfc68563304d9644e4ba8551
noam <noamsi@post.bgu.ac.il>
2020-03-01 13:58:34 +0200

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+import sys
+
 import cv2
 from math import sqrt
-
-
+from os import path
 def calc_distance(p1, p2):
     (x1, y1) = p1
     (x2, y2) = p2
@@ -25,71 +27,69 @@ def select_kp(event, x, y, flags, param):
             print("The requested point was not located. Please try again.")
         else:
             first_point = kp1[min_index]
-            cv2.imshow('Left_with_kp', Left_with_kp)
-            print("the closest point is at {} location {} with distance of {}".format(min_index, kp1[min_index].pt,
-                                                                                      minvalue))
 
 
 if __name__ == '__main__':
+    n = int(input("please insert n:\t"))
+    m = int(input("please insert m:\t"))
+    first_point = None
+    click_was_done = False
+    min_index = 0
 
-    n = 100
-    m = 10
-    # n = int(input("please insert n:\t"))
-    for i in [3]:
-        first_point = None
-        click_was_done = False
-        min_index = 0
+    if not path.exists("img_L.JPG") or not path.exists("img_R.JPG"):
+        print("The requested image files (img_L.jpg, img_R.jpg) could not be found. Exiting.", file=sys.stderr)
+        exit(-1)
+    Left = cv2.imread("img_L.JPG", cv2.IMREAD_COLOR)
+    Right = cv2.imread("img_R.JPG", cv2.IMREAD_COLOR)
 
-        img1 = cv2.imread("images/img{}_L.jpg".format(i), cv2.IMREAD_COLOR)
-        img2 = cv2.imread("images/img{}_R.JPG".format(i), cv2.IMREAD_COLOR)
-
-        scale_percent = 20  # percent of original size
-        width = int(img1.shape[1] * scale_percent / 100)
-        height = int(img1.shape[0] * scale_percent / 100)
+    rows, cols = Left.shape[:2]
+    if cols > 1024: #resizing image
+        scale_percent = 100 * 1024 / cols  # percent of original size
+        width = int(Left.shape[1] * scale_percent / 100)
+        height = int(Right.shape[0] * scale_percent / 100)
         dim = (width, height)
+        Left = cv2.resize(Left, dim, interpolation=cv2.INTER_AREA)
+        Right = cv2.resize(Right, dim, interpolation=cv2.INTER_AREA)
 
-        Left = cv2.resize(img1, dim, interpolation=cv2.INTER_AREA)
-        Right = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
+    orb = cv2.ORB_create(nfeatures=n)
+    kp1, des1 = orb.detectAndCompute(Left, None)
+    kp2, des2 = orb.detectAndCompute(Right, None)
 
-        orb = cv2.ORB_create(nfeatures=n)
-        kp1, des1 = orb.detectAndCompute(Left, None)
-        kp2, des2 = orb.detectAndCompute(Right, None)
+    Left_with_kp = cv2.drawKeypoints(Left, kp1, None)
+    param = [kp1]
+    cv2.namedWindow("Left_with_kp")
+    cv2.setMouseCallback("Left_with_kp", select_kp, param)
+    while True:
+        cv2.imshow('Left_with_kp', Left_with_kp)
+        cv2.waitKey(1)
 
-        Left_with_kp = cv2.drawKeypoints(Left, kp1, None)
-        param = [kp1]
-        cv2.namedWindow("Left_with_kp")
-        cv2.setMouseCallback("Left_with_kp", select_kp, param)
-        while True:
-            cv2.imshow('Left_with_kp', Left_with_kp)
-            cv2.waitKey(1)
+        if first_point is not None:
+            break
+    cv2.destroyAllWindows()
 
-            if first_point is not None:
-                break
-        cv2.destroyAllWindows()
+    """Part 2"""
+    first_point_des1 = des1[min_index:min_index + 1]
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    matches = bf.knnMatch(first_point_des1, des2, k=m)
+    matching_result = cv2.drawMatchesKnn(Left, [first_point], Right, kp2, matches, None, flags=2)
 
-        """Part 2"""
-        first_point_des1 = des1[min_index:min_index + 1]
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
-        matches = bf.knnMatch(first_point_des1, des2, k=m)
-        matching_result = cv2.drawMatchesKnn(Left, [first_point], Right, kp2, matches, None, flags=2)
+    cv2.imshow("One to Many Matching", matching_result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-        cv2.imshow("Matching Result", matching_result)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        """Part 3"""
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(des1, des2)
-        matches = sorted(matches, key=lambda x: x.distance)
-        with open("maximum_matching_cover.txt", "w+") as file:
-            toprint = ""
-            for m in matches:
-                p1 = kp1[m.queryIdx].pt
-                p2 = kp2[m.trainIdx].pt
-                toprint += "{} <--> {}\n".format(p1, p2)
-            file.write(toprint)
-        matched_image = cv2.drawMatches(Left, kp1, Right, kp2, matches, None, flags=2)
-        cv2.imshow("Maximum Matching", matched_image)
-        cv2.waitKey(0)
+    """Part 3"""
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    with open("maximum_matching_cover.txt", "w+") as file:
+        toprint = ""
+        for m in matches:
+            p1 = kp1[m.queryIdx].pt
+            p2 = kp2[m.trainIdx].pt
+            toprint += "{} <--> {}\n".format(p1, p2)
+        file.write(toprint)
+    matched_image = cv2.drawMatches(Left, kp1, Right, kp2, matches, None, flags=2)
+    cv2.imshow("Maximum Cover Matching", matched_image)
+    cv2.waitKey(0)
 
 
