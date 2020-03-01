be83d705c0d62500c396d325ad6cd42284bf241c
noam <noamsi@post.bgu.ac.il>
2020-03-01 13:01:19 +0200

    Commit

--- main.py
+++ main.py
@@ -2,27 +2,20 @@ import cv2
 from math import sqrt
 
 
-first_point = None
-click_was_done = False
-
-
 def calc_distance(p1, p2):
     (x1, y1) = p1
     (x2, y2) = p2
     return round(sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
 
 
-
 def select_kp(event, x, y, flags, param):
-    print("Entered select_kp")
-    global first_point
-    global click_was_done
+    global first_point, click_was_done, min_index
+
     if event == cv2.EVENT_LBUTTONDOWN:
         click_was_done = True
         kp1 = param[0]
         distances = []
         minvalue = 999999
-        min_index = 0
         for i in range(len(kp1)):
             distances.append(calc_distance((x, y), kp1[i].pt))
             if distances[i] < minvalue:
@@ -31,7 +24,7 @@ def select_kp(event, x, y, flags, param):
         if minvalue > 31:
             print("The requested point was not located. Please try again.")
         else:
-            first_point = kp1[min_index].pt
+            first_point = kp1[min_index]
             cv2.imshow('Left_with_kp', Left_with_kp)
             print("the closest point is at {} location {} with distance of {}".format(min_index, kp1[min_index].pt,
                                                                                       minvalue))
@@ -42,9 +35,13 @@ if __name__ == '__main__':
     n = 100
     m = 10
     # n = int(input("please insert n:\t"))
-    for i in range(1, 4):
-        img1 = cv2.imread("images/img{}_L.jpg".format(i), cv2.IMREAD_GRAYSCALE)
-        img2 = cv2.imread("images/img{}_R.JPG".format(i), cv2.IMREAD_GRAYSCALE)
+    for i in [3]:
+        first_point = None
+        click_was_done = False
+        min_index = 0
+
+        img1 = cv2.imread("images/img{}_L.jpg".format(i), cv2.IMREAD_COLOR)
+        img2 = cv2.imread("images/img{}_R.JPG".format(i), cv2.IMREAD_COLOR)
 
         scale_percent = 20  # percent of original size
         width = int(img1.shape[1] * scale_percent / 100)
@@ -60,26 +57,39 @@ if __name__ == '__main__':
 
         Left_with_kp = cv2.drawKeypoints(Left, kp1, None)
         param = [kp1]
-
+        cv2.namedWindow("Left_with_kp")
+        cv2.setMouseCallback("Left_with_kp", select_kp, param)
         while True:
-            cv2.setMouseCallback("Left_with_kp", select_kp, param)
-            while True:
-                cv2.imshow('Left_with_kp', Left_with_kp)
-                cv2.waitKey(1)
-                if click_was_done:
-                    click_was_done = False
-                    break
+            cv2.imshow('Left_with_kp', Left_with_kp)
+            cv2.waitKey(1)
+
             if first_point is not None:
                 break
-
         cv2.destroyAllWindows()
 
+        """Part 2"""
+        first_point_des1 = des1[min_index:min_index + 1]
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+        matches = bf.knnMatch(first_point_des1, des2, k=m)
+        matching_result = cv2.drawMatchesKnn(Left, [first_point], Right, kp2, matches, None, flags=2)
+
+        cv2.imshow("Matching Result", matching_result)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        """Part 3"""
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         matches = bf.match(des1, des2)
         matches = sorted(matches, key=lambda x: x.distance)
-
-        matching_result = cv2.drawMatches(Left, first_point, Right, kp2, matches[:m], None, flags=2)
-
-        cv2.imshow("Mtaching Result", matching_result)
+        with open("maximum_matching_cover.txt", "w+") as file:
+            toprint = ""
+            for m in matches:
+                p1 = kp1[m.queryIdx].pt
+                p2 = kp2[m.trainIdx].pt
+                toprint += "{} <--> {}\n".format(p1, p2)
+            file.write(toprint)
+        matched_image = cv2.drawMatches(Left, kp1, Right, kp2, matches, None, flags=2)
+        cv2.imshow("Maximum Matching", matched_image)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()
+
+
